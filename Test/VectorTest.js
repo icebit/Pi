b575539412f6ed62d13a566c2a892e215379edf1
icebit <epicpotatopro@gmail.com>
2016-01-22 13:24:45 -0600

    Update VectorTest.js

--- Test/VectorTest.js
+++ Test/VectorTest.js
@@ -1,6 +1,13 @@
 var assert = require('assert');
 var Vec2 = require("../Engine/Math/Vector").Vec2;
 describe("Vec2()", function(){
+  describe("Add()", function(){
+    it("Should return Vec2(2, 2) for Vec2(1, 1) + Vec2(1, 1)", function(){
+      var vec = Vec2(1, 1).Add(Vec2(1, 1));
+      assert.equal(vec.x, 2);
+      assert.equal(vec.y, 2);
+    });
+  });
   describe("GetLength()", function(){
     it("Should return 1.41421356 for x = 1, y = 1 rounded to eight decimals", function(){
       var vec = new Vec2(1, 1);
