ea264f2b1e067b61eb7b9ee0951bca5b1ebad54e
icebit <epicpotatopro@gmail.com>
2016-01-22 13:22:00 -0600

    Update Vector.js

--- Engine/Math/Vector.js
+++ Engine/Math/Vector.js
@@ -1,6 +1,11 @@
 exports.Vec2 = function(x, y){
   this.x = x;
   this.y = y;
+  
+  this.Add = function(other){
+    return exports.Vec2(this.x + other.x, this.y + other.y);
+  };
+  
   this.GetLength = function(){
     return Math.sqrt(x*x + y*y);
   };
